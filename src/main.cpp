30c9c401d215fa989c3d9a8a1d8ce20778c2d4d6
Christian Grumbein <christian@grumbein.de>
2021-08-30 20:08:38 +0200

    add helloworld for esp32

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Hello World\n");
+
+  // put your main code here, to run repeatedly:
+}
