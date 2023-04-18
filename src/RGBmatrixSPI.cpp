69c3decb5d5a8a75600ad2ee8eb9883162acdcd3
Christian Grumbein <christian@grumbein.de>
2023-04-18 22:16:16 +0200

    scrolling is now settable to an offset

--- src/RGBmatrixSPI.cpp
+++ src/RGBmatrixSPI.cpp
@@ -43,7 +43,7 @@ void RGBmatrixSPI::transfer() {
   spi.transfer(static_cast<uint8_t>(Registers::transfer));
   spi.transfer(0x00);
   spi.transfer(0x00);
-  for (int i = 0; i < matrix_width*matrix_height*2; i++) {
+  for (int i = 0; i < (matrix_width*matrix_height << 1); i++) {
     spi.transfer(*(transferBuffer+i));
   }
   digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer
@@ -59,43 +59,37 @@ void RGBmatrixSPI::transfer() {
   }
 }
 
-void RGBmatrixSPI::scroll() {
+void RGBmatrixSPI::scroll( uint32_t scroll) {
 
-  for (int j = 0; j < matrix_width; j++) {
-    spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
-    digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
-    spi.transfer(static_cast<uint8_t>(Registers::transfer));
-    spi.transfer(0x00);
-    spi.transfer(0x00);
-    for (int row = 0; row < matrix_width; row++) {
-      for (int column = 0; column < matrix_height; column++) {
-        if (row < 20) {
-          spi.transfer(*(transferBuffer+ (row * matrix_height + column)*2));
-          spi.transfer(*(transferBuffer+ (row * matrix_height + column)*2 + 1));
+  spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
+  digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
+  spi.transfer(static_cast<uint8_t>(Registers::transfer));
+  spi.transfer(0x00);
+  spi.transfer(0x00);
+  for (int row = 0; row < matrix_width; row++) {
+    for (int column = 0; column < matrix_height; column++) {
+      if (row < 20) {
+        spi.transfer(*(transferBuffer+ ((row * matrix_height + column) << 1)));
+        spi.transfer(*(transferBuffer+ ((row * matrix_height + column) << 1) + 1));
+      } else {
+        if (column < scroll) {
+          // new data
+          spi.transfer(*(transferBuffer+ ((row * matrix_height + column + 64 - scroll) << 1)));
+          spi.transfer(*(transferBuffer+ ((row * matrix_height + column + 64 - scroll) << 1) + 1));
         } else {
-          if (column < j) {
-            // new data
-            spi.transfer(*(transferBuffer+ (row * matrix_height + column + 64 - j)*2));
-            spi.transfer(*(transferBuffer+ (row * matrix_height + column + 64 - j)*2 + 1));
-          } else {
-            spi.transfer(*(transferBuffer+ (matrix_height*(matrix_width-1) +
-                                            row * matrix_height + column + 64 - j)*2));
-            spi.transfer(*(transferBuffer+ (matrix_height*(matrix_width-1) +
-                                            row * matrix_height + column + 64 - j)*2 + 1));
-          }
+          spi.transfer(*(transferBuffer+ ((matrix_height*(matrix_width-1) +
+                                          row * matrix_height + column + 64 - scroll) << 1)));
+          spi.transfer(*(transferBuffer+ ((matrix_height*(matrix_width-1) +
+                                          row * matrix_height + column + 64 - scroll) << 1) + 1));
         }
       }
     }
-    digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer
-    spi.endTransaction();//
-    spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
-    digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
-    spi.transfer(static_cast<uint8_t>(Registers::buffer_switch));
-    digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer                               //
-    spi.endTransaction();
   }
-  // safe this buffer for next scoll action
-  for (int i = 0; i < matrix_width*matrix_height; i++) {
-    *(frameBuffer + i + matrix_width*matrix_height) = *(frameBuffer + i);
-  }
+  digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer
+  spi.endTransaction();//
+  spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
+  digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
+  spi.transfer(static_cast<uint8_t>(Registers::buffer_switch));
+  digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer                               //
+  spi.endTransaction();
 }
