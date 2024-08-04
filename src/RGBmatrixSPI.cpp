cd7c76a4be900afba5ab30b144e82aa83b0cab49
Christian Grumbein <christian@grumbein.de>
2024-08-04 07:05:30 +0200

    clang-tidy code changes

--- src/RGBmatrixSPI.cpp
+++ src/RGBmatrixSPI.cpp
@@ -1,31 +1,31 @@
 #include "RGBmatrixSPI.h"
-#include "stdint.h"
 
-#include <stdlib.h>     /* malloc, free, rand */
-#include <sys/types.h>
+#include <cstdint>
+#include <cstdlib>
 
-RGBmatrixSPI::RGBmatrixSPI(int16_t width, int16_t height, int speed)
-    : Adafruit_GFX(width, height), matrix_width(width), matrix_height(height),
-      spi_speed(speed), spi(VSPI) {
+RGBmatrixSPI::RGBmatrixSPI(int speed, int rotation)
+    : Adafruit_GFX(MATRIX_WIDTH, MATRIX_HEIGHT), spi_speed(speed), spi(VSPI),
+      rotation(rotation) {
   Serial.println("Initializing");
   spi.begin();
   pinMode(VSPI_SS, OUTPUT);
-  frameBuffer = (uint16_t*)malloc(width * height*4);
-  transferBuffer =(uint8_t*)frameBuffer;
+  frameBuffer = (uint16_t*)malloc(MATRIX_HEIGHT * MATRIX_WIDTH*4);
+  backBuffer = (uint16_t*)malloc(MATRIX_HEIGHT * MATRIX_WIDTH*2);
+  transferBuffer =(uint8_t*)backBuffer;
 }
 void RGBmatrixSPI::drawPixel(int16_t x, int16_t y, uint16_t c) {
-  uint16_t address = (x + matrix_width * y);
+  uint16_t address = (x + MATRIX_WIDTH * y);
   *(frameBuffer + address) = c;
 }
 
 void RGBmatrixSPI::fillScreen(uint16_t color){
-  for (int i = 0; i < matrix_width*matrix_height; i++) {
+  for (int i = 0; i < MATRIX_WIDTH*MATRIX_HEIGHT; i++) {
     *(frameBuffer + i) = color;
   }
 }
 
-void RGBmatrixSPI::fillClock(uint16_t color){
-  for (int i = 0; i < matrix_width*20; i++) {
+void RGBmatrixSPI::fillClock(uint16_t color) const{
+  for (int i = 0; i < MATRIX_WIDTH*20; i++) {
     *(frameBuffer + i) = color;
   }
 }
@@ -38,13 +38,37 @@ void RGBmatrixSPI::setBrightness(int8_t brightness) {
   spi.endTransaction();
 }
 void RGBmatrixSPI::transfer() {
+  uint16_t x_rot, y_rot;
+  for (int y = 0; y < MATRIX_HEIGHT; y++) {
+    for (int x = 0; x < MATRIX_WIDTH; x++) {
+      switch (rotation) {
+      case 0:
+        x_rot = x;
+        y_rot = y;
+        break;
+      case 1:
+        x_rot = MATRIX_WIDTH - 1 - y;
+        y_rot = x;
+        break;
+      case 2:
+        x_rot = MATRIX_WIDTH - 1 - x;
+        y_rot = MATRIX_HEIGHT - 1 - y;
+        break;
+      case 3:
+        x_rot = y;
+        y_rot = MATRIX_HEIGHT - 1 - x;
+        break;
+      }
+      backBuffer[x_rot + y_rot * MATRIX_WIDTH] = frameBuffer[x + y * MATRIX_WIDTH];
+    }
+  }
   spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
   digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
   spi.transfer(static_cast<uint8_t>(Registers::transfer));
   spi.transfer(0x00);
   spi.transfer(0x00);
-  for (int i = 0; i < (matrix_width*matrix_height << 1); i++) {
-    spi.transfer(*(transferBuffer+i));
+  for (int i = 0; i < (MATRIX_WIDTH*MATRIX_HEIGHT << 1); i++) {
+    spi.transfer(transferBuffer[i]);
   }
   digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer
   spi.endTransaction();//
@@ -54,36 +78,53 @@ void RGBmatrixSPI::transfer() {
   digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer                               //
   spi.endTransaction();
   //safe this buffer for next scroll action
-  for (int i = 0; i < matrix_width*matrix_height; i++) {
-    *(frameBuffer + i + matrix_width*matrix_height) = *(frameBuffer + i);
+  for (int i = 0; i < MATRIX_WIDTH*MATRIX_HEIGHT; i++) {
+    *(frameBuffer + i + MATRIX_WIDTH*MATRIX_HEIGHT) = *(frameBuffer + i);
   }
 }
 
 void RGBmatrixSPI::scroll( uint32_t scroll) {
-
+  uint16_t x_rot, y_rot;
+  for (int y = 0; y < MATRIX_HEIGHT; y++) {
+    for (int x = 0; x < MATRIX_WIDTH; x++) {
+      switch (rotation) {
+      case 0:
+        x_rot = x;
+        y_rot = y;
+        break;
+      case 1:
+        x_rot = MATRIX_WIDTH - 1 - y;
+        y_rot = x;
+        break;
+      case 2:
+        x_rot = MATRIX_WIDTH - 1 - x;
+        y_rot = MATRIX_HEIGHT - 1 - y;
+        break;
+      case 3:
+        x_rot = y;
+        y_rot = MATRIX_HEIGHT - 1 - x;
+        break;
+      }
+      if (y < 20) {
+        backBuffer[y_rot * MATRIX_HEIGHT + x_rot] = frameBuffer[y * MATRIX_HEIGHT + x];
+      } else {
+        if (x < scroll) {
+          // new data
+          backBuffer[y_rot * MATRIX_HEIGHT + x_rot] = frameBuffer[y * MATRIX_HEIGHT + x + 64 - scroll];
+        } else {
+          backBuffer[y_rot * MATRIX_HEIGHT + x_rot] = frameBuffer[(MATRIX_HEIGHT*(MATRIX_WIDTH-1) +
+                                                           y * MATRIX_HEIGHT + x + 64 - scroll)];
+        }
+      }
+    }
+  }
   spi.beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE0));
   digitalWrite(VSPI_SS, LOW); // pull SS slow to prep other end for transfer
   spi.transfer(static_cast<uint8_t>(Registers::transfer));
   spi.transfer(0x00);
   spi.transfer(0x00);
-  for (int row = 0; row < matrix_width; row++) {
-    for (int column = 0; column < matrix_height; column++) {
-      if (row < 20) {
-        spi.transfer(*(transferBuffer+ ((row * matrix_height + column) << 1)));
-        spi.transfer(*(transferBuffer+ ((row * matrix_height + column) << 1) + 1));
-      } else {
-        if (column < scroll) {
-          // new data
-          spi.transfer(*(transferBuffer+ ((row * matrix_height + column + 64 - scroll) << 1)));
-          spi.transfer(*(transferBuffer+ ((row * matrix_height + column + 64 - scroll) << 1) + 1));
-        } else {
-          spi.transfer(*(transferBuffer+ ((matrix_height*(matrix_width-1) +
-                                          row * matrix_height + column + 64 - scroll) << 1)));
-          spi.transfer(*(transferBuffer+ ((matrix_height*(matrix_width-1) +
-                                          row * matrix_height + column + 64 - scroll) << 1) + 1));
-        }
-      }
-    }
+  for (int i = 0; i < (MATRIX_WIDTH*MATRIX_HEIGHT << 1); i++) {
+    spi.transfer(*(transferBuffer+i));
   }
   digitalWrite(VSPI_SS, HIGH); // pull ss high to signify end of data transfer
   spi.endTransaction();//
--- src/RGBmatrixSPI.h
+++ src/RGBmatrixSPI.h
@@ -1,37 +1,35 @@
 #ifndef RGBMATRIXSPI_H
 #define RGBMATRIXSPI_H
 #include "Adafruit_GFX.h"
-#include "stdint.h"
+#include <cstdint>
 #include <SPI.h>
-#define VSPI_MISO MISO
-#define VSPI_MOSI MOSI
-#define VSPI_SCLK SCK
 #define VSPI_SS SS
+#define MATRIX_HEIGHT 64
+#define MATRIX_WIDTH 64
 
 class RGBmatrixSPI : public Adafruit_GFX {
+
 public:
-  RGBmatrixSPI(int16_t width, int16_t height, int speed);
-  void drawPixel(int16_t x, int16_t y, uint16_t c);
+  RGBmatrixSPI(int speed, int rotation);
+  void drawPixel(int16_t x, int16_t y, uint16_t c) override;
   void setBrightness(int8_t brightness);
-  void fillScreen(uint16_t color);
-  void fillClock(uint16_t color);
+  void fillScreen(uint16_t color) override;
+  void fillClock(uint16_t color) const;
   uint16_t *frameBuffer;
+  uint16_t *backBuffer;
   uint8_t *transferBuffer;
   void transfer();
   void scroll(uint32_t scroll);
 
 private:
-  const int matrix_width;
-  const int matrix_height;
   const int spi_speed;
   SPIClass spi;
+  int rotation = 0;
   enum class Registers : std::uint8_t {
     brightness = 0xf1,
     transfer = 0xf3,
     buffer_switch = 0xf4
   };
-  const uint16_t lower_bytes = 0x00ff;
-  const uint16_t upper_shift = 8;
 };
 
 #endif
